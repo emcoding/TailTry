d5328b563ef24813d75a3560a726d2c5ac5241cf
Maud de Vries <emcoding@pm.me>
2021-07-26 15:13:13 +0200

    Aanpassen theme kleur en texten

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      
+        accent: "#2E8B57",
+        brand: "#0f766e",
+        emerald: "#047857"
+      }
+    },
   },
   variants: {
     extend: {},
